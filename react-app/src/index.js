b6dfaa265afa0a3a17d4a85bee226deca0286198
lfabrikant <Leonid.Fabrikant@ehi.com>
2019-03-21 01:23:37 -0400

    react-app

---
+++ react-app/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+ReactDom.render(
+    <div>Hello World</div>,
+    document.getElementById('app')
+);
---
+++ react-app/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  watch: true,
+  devServer: {
+    //contentBase: '../dist',
+    compress: true,
+    port: 9000
+  }
+};
