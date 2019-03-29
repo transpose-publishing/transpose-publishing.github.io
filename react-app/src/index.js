0c0ef5c52ef3fc25cb6d7b7ed36b071af5c0cfd6
lfabrikant <Leonid.Fabrikant@ehi.com>
2019-03-29 02:22:33 -0400

    react-app: added new library to fetch data sheetrock. commented out old library for now

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,22 +3,59 @@ import ReactDom from 'react-dom';
 import {HashRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import HomePage from './homePage';
 
+const startTime = Date.now();
 const spreadSheetUrl = 'https://docs.google.com/spreadsheets/d/19fuw6MEVPgoTgqY5Vh8JFEvW_HA4oPaDE_g3BZhB7Ek/edit?usp=sharing';
+
+function fetchData ({rows = 0}) {
+  return new Promise((resolve, reject) => {
+    sheetrock({
+      url: spreadSheetUrl + "#gid=0",
+      fetchSize: rows,
+      callback: (errors, optiosn, resp) => {
+        const dataArray = [];
+        for (const row of resp.rows) {
+          if(row.num !== 0) {
+            const item = {};
+            let labelIndex = 0;
+            for (const label of row.labels) {
+              item[label] = row.cellsArray[labelIndex];
+              labelIndex++
+            }
+            dataArray.push(item);
+          }
+        }
+        console.log('timer', Date.now() - startTime);
+        resolve(dataArray);
+      }
+    });
+  });
+}
 
 function App () {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
   const [error, setError] = useState();
 
+  // useEffect(() => {
+  //   Tabletop.init({
+  //     key: spreadSheetUrl,
+  //     callback: function(data) {
+  //       console.log('time', Date.now() - startTime);
+  //       setLoading(false);
+  //       setData(data);
+  //     },
+  //     simpleSheet: true
+  //   });
+  // }, []);
+
   useEffect(() => {
-    Tabletop.init({
-      key: spreadSheetUrl,
-      callback: function(data) {
+    fetchData({rows: 50})
+      .then( dataArray => {
+        setData(dataArray);
         setLoading(false);
-        setData(data);
-      },
-      simpleSheet: true
-    });
+        fetchData({rows: 0}) //0 fetches all data
+          .then( dataArray => setData(dataArray))
+      })
   }, []);
 
   return (
