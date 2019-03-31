bad81499306efceb3731cd986e5941714e41f20f
lfabrikant <Leonid.Fabrikant@ehi.com>
2019-03-31 19:02:00 -0400

    react-app: fixed page-arrow path

--- react-app/src/paging.js
+++ react-app/src/paging.js
@@ -8,7 +8,7 @@ export default function Paging ({page, totalPages, setPage}) {
   return (
     <div className="paging-container">
       <button onClick={() => page > 0 && setPage(page - 1)}>
-        <img className="page-arrow left-arrow" src={'../assets/Icons/page-arrow.png'}/>
+        <img className="page-arrow left-arrow" src={'./assets/Icons/page-arrow.png'}/>
       </button>
 
       {pagesArray.map( (_, i) =>
@@ -22,7 +22,7 @@ export default function Paging ({page, totalPages, setPage}) {
       )}
 
       <button onClick={() => page < totalPages && setPage(page + 1)}>
-        <img className="page-arrow right-arrow" src={'../assets/Icons/page-arrow.png'}/>
+        <img className="page-arrow right-arrow" src={'./assets/Icons/page-arrow.png'}/>
       </button>
     </div>
   )
