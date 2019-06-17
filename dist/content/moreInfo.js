97aa57fc808b4de40e2d9f4c5fcb6e13d4f59aaf
Jessica Polka <jessica.polka@gmail.com>
2019-06-17 17:44:59 -0400

    Add inline link to data download

--- dist/content/moreInfo.js
+++ dist/content/moreInfo.js
@@ -214,8 +214,13 @@ will render a card in the FAQ section. FAQ Descriptions can also include in_link
 window._content.faq = [
   {
     title: 'Where can I access the data?',
-    description: 'You can download all of the data here.'
-  },
+    description: {
+      text: 'You can download all of the data here.',
+      in_line_link: {
+        text: 'here',
+        link: 'https://docs.google.com/spreadsheets/d/10cgryddPROS2szJrC2xzTsqEu1nCgSTNZZBkLizZfTc/export?format=csv&id=10cgryddPROS2szJrC2xzTsqEu1nCgSTNZZBkLizZfTc&gid=0'
+      }
+      },
   {
     title: 'I am a publisher interested in depositing or updating data; whom should I contact?',
     description: {
