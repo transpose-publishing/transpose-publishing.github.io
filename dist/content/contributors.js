001613ce141346f9d97c63b51578f9a4880a6eff
Jessica Polka <jessica.polka@gmail.com>
2019-10-15 13:08:35 -0400

    Update contributors.js
    Adding Sarah & PAST/CURRENT subheadings

--- dist/content/contributors.js
+++ dist/content/contributors.js
@@ -5,12 +5,15 @@ will be rendered in the About page.
 */
 
 window._content.contributorsList = [
+  'CURRENT',
   'Samantha Hindle, bioRxiv and PREreview, USA',
   'Jennifer Lin, Crossref, USA',
   'Gary McDowell, Future of Research, USA',
   'Jessica Polka, ASAPbio, USA',
   'Naomi Penfold, ASAPbio, USA',
-  'Tony Ross-Hellauer, Know-Center, Austria',
+  'Tony Ross-Hellauer, TU Graz & Know-Center, Austria',
+  'Sarah Stryeck, TU Graz, Austria',
+  'PAST',
   'Cameron Blandford, 221B, USA',
   'Benedikt Fecher, Humboldt Institute for Internet and Society, Germany',
   'Daniel Himmelstein, UPenn, USA',
